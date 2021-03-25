3b1da69f71da0acecb00d6cea6445044affd8c07
bryjch <bryanch7@gmail.com>
2021-03-25 21:42:35 +0800

    typescriptify utils

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,6 +1,6 @@
 // Simple util to sleep x milliseconds
 
-export const sleep = millis =>
+export const sleep = (millis: number) =>
   new Promise(resolve => {
     return setTimeout(resolve, millis)
   })
