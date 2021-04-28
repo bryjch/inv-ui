55099800e61a4570aea7aa0874830df0ae3eea2e
bryjch <bryjch@gmail.com>
2021-04-28 17:29:33 +0800

    improve sidebar

--- src/shared/components/Sidebar.tsx
+++ src/shared/components/Sidebar.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaGithub } from 'react-icons/fa'
+import { FiMenu } from 'react-icons/fi'
 
 import { dispatch, useStore } from '@zus/store'
 import { toggleSidebarAction, setActiveGameAction } from '@zus/actions'
 
 import { hexToRgba } from '@utils/styling'
-import { GITHUB_URL } from '@constants/config'
+import { useEventListener } from '@utils/hooks'
 
-import { GAMES } from '@constants/config'
+import { GITHUB_URL, GAMES } from '@constants/config'
 
 export const Sidebar = () => {
+  const [isMobile, setIsMobile] = useState(false)
   const isOpen = useStore(state => state.ui.sidebarOpen)
   const activeGame = useStore(state => state.app.activeGame)
 
@@ -17,44 +19,58 @@ export const Sidebar = () => {
   // ─── LIFECYCLE ──────────────────────────────────────────────────────────────────
   //
 
-  const onMouseEnter = () => dispatch(toggleSidebarAction(true))
-  const onMouseLeave = () => dispatch(toggleSidebarAction(false))
+  const onToggleBurger = () => dispatch(toggleSidebarAction())
+  const onMouseEnter = () => !isMobile && dispatch(toggleSidebarAction(true))
+  const onMouseLeave = () => !isMobile && dispatch(toggleSidebarAction(false))
+
+  useEventListener('resize', () => {
+    if (window.innerWidth < 768) {
+      if (!isMobile) setIsMobile(true)
+    } else {
+      if (isMobile) setIsMobile(false)
+    }
+  })
 
   //
   // ─── RENDER ─────────────────────────────────────────────────────────────────────
   //
 
-  return (
-    <div
-      id="sidebar"
-      className={isOpen ? 'open' : ''}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-    >
-      <div className="options">
-        {GAMES.map(game => (
-          <div
-            key={`game-swap-option-${game.id}`}
-            className={`option ${activeGame?.id === game.id ? 'active' : ''}`}
-            onClick={() => dispatch(setActiveGameAction(game))}
-          >
-            <img src={game.image} alt={game.name} />
+  const cls = []
+  if (isMobile) cls.push('mobile')
+  if (isOpen) cls.push('open')
 
-            <div className="name">{game.name}</div>
-          </div>
-        ))}
+  return (
+    <div id="sidebar" className={cls.join(' ')}>
+      <div className="burger" onClick={onToggleBurger}>
+        <FiMenu size={30} color="#ffffff" />
       </div>
 
-      <div className="others">
-        <a
-          className="external-link github"
-          href={GITHUB_URL}
-          title="Github"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaGithub size={26} color="#ffffff" />
-        </a>
+      <div className="main-panel" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+        <div className="options">
+          {GAMES.map(game => (
+            <div
+              key={`game-swap-option-${game.id}`}
+              className={`option ${activeGame?.id === game.id ? 'active' : ''}`}
+              onClick={() => dispatch(setActiveGameAction(game))}
+            >
+              <img src={game.image} alt={game.name} />
+
+              <div className="name">{game.name}</div>
+            </div>
+          ))}
+        </div>
+
+        <div className="others">
+          <a
+            className="external-link github"
+            href={GITHUB_URL}
+            title="Github"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size={26} color="#ffffff" />
+          </a>
+        </div>
       </div>
 
       <style jsx>{`
@@ -63,123 +79,157 @@ export const Sidebar = () => {
           flex-flow: column nowrap;
           justify-content: flex-start;
           align-items: center;
-          width: ${Sidebar.WIDTH}px;
+          pointer-events: none;
           position: absolute;
           z-index: 99999;
-          padding: 0.5rem;
-          top: 0;
-          bottom: 0;
-          left: ${-Sidebar.WIDTH}px;
-          transition: 0.5s ease all;
+          top: 1rem;
+          bottom: 1rem;
+          left: 1rem;
 
-          /* Use :after to make an invisible hoverable area */
-          &:after {
-            content: '';
+          .burger {
+            display: none;
             position: absolute;
-            height: 100%;
             top: 0;
-            left: ${Sidebar.WIDTH}px;
+            left: 0;
+            pointer-events: auto;
+            cursor: pointer;
+            background-color: ${hexToRgba('#202225', 1)};
+            border-radius: 12px;
+            justify-content: center;
+            align-items: center;
+            width: 50px;
+            height: 50px;
+          }
+
+          .main-panel {
+            display: flex;
+            flex-flow: column nowrap;
+            justify-content: flex-start;
+            align-items: center;
             width: ${Sidebar.WIDTH}px;
-            transition: inherit;
+            padding: 1rem 0;
+            margin: auto 0;
+            background-color: ${hexToRgba('#202225', 0.9)};
+            border-radius: 12px;
+            transform: scale(0.66);
+            transform-origin: left center;
+            transition: 0.5s ease all;
+            pointer-events: auto;
           }
 
           &.open {
-            left: 0;
-            background-color: #202225;
+            .main-panel {
+              left: 0;
+              background-color: #202225;
+              transform: scale(1);
+            }
+          }
+
+          &.mobile {
+            .burger {
+              display: flex;
+            }
+
+            .main-panel {
+              margin-top: 80px;
+              margin-left: -${Sidebar.WIDTH * 2}px;
+              transform: scale(1);
+            }
+          }
+
+          &.mobile.open {
+            .main-panel {
+              margin-left: 0;
+            }
+          }
+        }
+
+        .options {
+          padding-bottom: 1rem;
+
+          .option {
+            position: relative;
+            cursor: pointer;
+            line-height: 0;
+            padding: 5px;
+            border-radius: 50%;
+            margin: 0.5rem 0;
+            transition: 0.3s ease all;
+            user-select: none;
+
+            img {
+              width: 50px;
+              height: 50px;
+              image-rendering: -webkit-optimize-contrast;
+            }
+
+            .name {
+              position: absolute;
+              top: calc(50% - 15px);
+              left: ${Sidebar.WIDTH - 20}px;
+              color: #ffffff;
+              background-color: rgba(0, 0, 0, 0.8);
+              height: 30px;
+              line-height: 30px;
+              padding: 0 15px;
+              font-size: 0.8rem;
+              letter-spacing: 2px;
+              border-radius: 4px;
+              text-transform: uppercase;
+              opacity: 0;
+              pointer-events: none;
+              transition: 0.3s ease all;
+            }
 
             &:after {
-              width: 0;
-            }
-          }
-
-          .options {
-            margin: auto;
-
-            .option {
-              position: relative;
-              cursor: pointer;
-              line-height: 0;
-              padding: 5px;
+              content: '';
+              position: absolute;
+              width: 100%;
+              height: 100%;
+              top: 0;
+              right: 0;
+              bottom: 0;
+              left: 0;
               border-radius: 50%;
-              margin: 0.5rem 0;
+              border: 1px dashed rgba(255, 255, 255, 0.2);
               transition: 0.3s ease all;
-              user-select: none;
+            }
 
-              img {
-                width: 50px;
-                height: 50px;
-                image-rendering: -webkit-optimize-contrast;
-              }
+            &:hover {
+              background-color: ${hexToRgba('#b4c7ec', 0.5)};
+              transform: scale(1.1);
 
               .name {
-                position: absolute;
-                top: calc(50% - 15px);
-                left: ${Sidebar.WIDTH - 20}px;
-                color: #ffffff;
-                background-color: rgba(0, 0, 0, 0.8);
-                height: 30px;
-                line-height: 30px;
-                padding: 0 15px;
-                font-size: 0.8rem;
-                letter-spacing: 2px;
-                text-transform: uppercase;
-                opacity: 0;
-                pointer-events: none;
-                transition: 0.3s ease all;
-              }
-
-              &:after {
-                content: '';
-                position: absolute;
-                width: 100%;
-                height: 100%;
-                top: 0;
-                right: 0;
-                bottom: 0;
-                left: 0;
-                border-radius: 50%;
-                border: 1px dashed rgba(255, 255, 255, 0.2);
-                transition: 0.3s ease all;
-              }
-
-              &:hover {
-                background-color: ${hexToRgba('#b4c7ec', 0.5)};
-                transform: scale(1.1);
-
-                .name {
-                  opacity: 1;
-                  left: ${Sidebar.WIDTH - 5}px;
-                  transform: scale(0.9);
-                }
-              }
-
-              &.active {
-                &:after {
-                  border: 2px solid #b4c7ec;
-                }
-              }
-
-              &:hover.active {
-                background-color: ${hexToRgba('#b4c7ec', 1)};
+                opacity: 1;
+                left: ${Sidebar.WIDTH - 10}px;
+                transform: scale(0.9);
               }
             }
-          }
 
-          .others {
-            margin-bottom: 0.5rem;
-
-            .external-link {
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              border-radius: 50%;
-              width: 40px;
-              height: 40px;
-              transition: 0.3s ease all;
-
-              &:hover {
-                background-color: ${hexToRgba('#b4c7ec', 1)};
+            &.active {
+              &:after {
+                border: 2px solid #b4c7ec;
               }
+            }
+
+            &:hover.active {
+              background-color: ${hexToRgba('#b4c7ec', 1)};
+            }
+          }
+        }
+
+        .others {
+          .external-link {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 50%;
+            width: 40px;
+            height: 40px;
+            transition: 0.3s ease all;
+
+            &:hover {
+              background-color: ${hexToRgba('#b4c7ec', 1)};
             }
           }
         }
