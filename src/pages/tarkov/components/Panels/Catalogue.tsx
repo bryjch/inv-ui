9cd6ca4cc5a7adea40c0f80792b2da30f86d2ff6
bryjch <bryanch7@gmail.com>
2021-05-17 11:07:55 +0800

    add catalogue panel overflow

--- src/pages/tarkov/components/Panels/Catalogue.tsx
+++ src/pages/tarkov/components/Panels/Catalogue.tsx
@@ -6,8 +6,27 @@ export interface CatalogueProps {
 
 export const Catalogue = (props: CatalogueProps) => {
   return (
-    <div className="panel">
-      <Listing id="listing-catalogue" {...props.areaMethods} />
+    <div id="catalogue" className="panel">
+      <div className="panel-title">CATALOGUE</div>
+
+      <div className="listings">
+        <Listing id="listing-catalogue" {...props.areaMethods} />
+      </div>
+
+      <style jsx>{`
+        #catalogue {
+          flex: 1;
+
+          .listings {
+            display: flex;
+            flex-flow: column nowrap;
+            align-items: flex-start;
+            width: 100%;
+            overflow-y: auto;
+            padding-right: 1rem;
+          }
+        }
+      `}</style>
     </div>
   )
 }
